c4aba3dd1727d858155df5e383bcfd8e82e56989
Umair <omerjerk@gmail.com>
2013-01-22 19:37:36 +0530

    Added an About dialog check9ing how this works
    Create README.md
    
    Revert "check9ing how this works"
    
    This reverts commit ffb85e26fbf9d5d55f927392c3c31fca22e79d61.
    
    Just some small changes
    
    Added some changes
    
    deleted the previously created unused files
    
    Added an about option in Main page
    
    Look of About dialog changed and a close button added.
    
    changes
    
    Revert "changes"
    
    This reverts commit 9827723b4dd8770a85dcf5e19b846b2fdbb78cc5.
    
    sorry for repeating the same commits .. It was due to some problem ..
    
    updated to 0.5.4
    
    Update README.md
    
    Added the actual description of Troika

--- src/com/dcetech/troika/Events.java
+++ src/com/dcetech/troika/Events.java
@@ -3,23 +3,14 @@ package com.dcetech.troika;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.provider.ContactsContract;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Intent;
-import android.util.Log;
 import android.view.Gravity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
-import android.widget.RadioButton;
 import android.widget.ScrollView;
 import android.widget.Toast;
-import android.widget.ViewFlipper;
 
 
 public class Events extends Activity {
