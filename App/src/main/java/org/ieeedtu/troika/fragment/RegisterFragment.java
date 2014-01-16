a70160186cf2bbe09784b453998ca4ea9d6a3aab
Arnav Gupta <championswimmer@aokp.co>
2014-01-16 17:52:57 +0530

    add registration code and finish details
    Signed-off-by: Arnav Gupta <championswimmer@aokp.co>

--- App/src/main/java/org/ieeedtu/troika/fragment/RegisterFragment.java
+++ App/src/main/java/org/ieeedtu/troika/fragment/RegisterFragment.java
@@ -1,14 +1,33 @@
 package org.ieeedtu.troika.fragment;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 import org.ieeedtu.troika.R;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -20,6 +39,12 @@ import org.ieeedtu.troika.R;
  * create an instance of this fragment.
  */
 public class RegisterFragment extends Fragment {
+
+    private String registerName = new String();
+    private String registerEmail = new String();
+    private String registerPhone = new String();
+    private String registerTeam = new String();
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -53,6 +78,7 @@ public class RegisterFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @SuppressLint("NewApi")
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,13 +86,57 @@ public class RegisterFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_register, container, false);
+        final EditText rName, rEmail, rPhone, rTeam;
+        rName = (EditText) rootView.findViewById(R.id.register_name);
+        rEmail = (EditText) rootView.findViewById(R.id.register_email);
+        rPhone = (EditText) rootView.findViewById(R.id.register_phone);
+        rTeam = (EditText) rootView.findViewById(R.id.register_teamname);
+
+        Button submit = (Button) rootView.findViewById(R.id.register_submit);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registerName = rName.getText().toString();
+                registerEmail = rEmail.getText().toString();
+                registerPhone = rPhone.getText().toString();
+                registerTeam = rTeam.getText().toString();
+
+                HttpClient submitClient = new DefaultHttpClient();
+                HttpPost submitPost = new HttpPost("http://troika.dcetech.com/register/submit.php");
+                List<NameValuePair> postPair = new ArrayList<NameValuePair>();
+                postPair.add(new BasicNameValuePair("name", registerName));
+                postPair.add(new BasicNameValuePair("email", registerEmail));
+                postPair.add(new BasicNameValuePair("phone", registerPhone));
+                postPair.add(new BasicNameValuePair("teamname", registerTeam));
+                try {
+                    submitPost.setEntity(new UrlEncodedFormEntity(postPair));
+                    submitClient.execute(submitPost);
+                    //HttpResponse submitResponse = submitClient.execute(submitPost);
+                    //String response = EntityUtils.toString(submitResponse.getEntity());
+                    //Log.d("TROIKA", response);
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                } catch (ClientProtocolException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+        return rootView;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
